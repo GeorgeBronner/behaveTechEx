7c5f4494e67106f8e3de05c76d109ebf857db5e1
George Bronner <george.bronner@gmail.com>
2023-04-02 13:46:53 -0400

    BasePage refactor

--- features/pageobjects/BasePage.py
+++ features/pageobjects/BasePage.py
@@ -8,43 +8,25 @@ class BasePage:
         self.driver = driver
 
     def select(self, locator):
-        self.driver.find_element(By.CLASS_NAME, configReader.readConfig("locators", locator))
+        if str(locator).endswith("_XPATH"):
+            return self.driver.find_element(By.XPATH, configReader.readConfig("locators", locator))
+        elif str(locator).endswith("_CSS"):
+            return self.driver.find_element(By.CSS_SELECTOR, configReader.readConfig("locators", locator))
+        elif str(locator).endswith("_ID"):
+            return self.driver.find_element(By.ID, configReader.readConfig("locators", locator))
+        elif str(locator).endswith("_CLASS"):
+            return self.driver.find_element(By.CLASS_NAME, configReader.readConfig("locators", locator))
+        elif str(locator).endswith("_NAME"):
+            return self.driver.find_element(By.NAME, configReader.readConfig("locators", locator))
 
     def selectText(self, locator):
-        return self.driver.find_element(By.XPATH, configReader.readConfig("locators", locator)).text
+        return self.select(locator).text
 
     def click(self, locator):
-        if str(locator).endswith("_XPATH"):
-            self.driver.find_element(By.XPATH, configReader.readConfig("locators", locator)).click()
-        elif str(locator).endswith("_CSS"):
-            self.driver.find_element(By.CSS_SELECTOR, configReader.readConfig("locators", locator)).click()
-        elif str(locator).endswith("_ID"):
-            self.driver.find_element(By.ID, configReader.readConfig("locators", locator)).click()
-        elif str(locator).endswith("_CLASS"):
-            self.driver.find_element(By.CLASS_NAME, configReader.readConfig("locators", locator)).click()
-        elif str(locator).endswith("_NAME"):
-            self.driver.find_element(By.NAME, configReader.readConfig("locators", locator)).click()
+        return self.select(locator).click()
 
     def type(self, locator, value):
-        if str(locator).endswith("_XPATH"):
-            self.driver.find_element(By.XPATH, configReader.readConfig("locators", locator)).send_keys(value)
-        elif str(locator).endswith("_CSS"):
-            self.driver.find_element(By.CSS_SELECTOR, configReader.readConfig("locators", locator)).send_keys(value)
-        elif str(locator).endswith("_ID"):
-            self.driver.find_element(By.ID, configReader.readConfig("locators", locator)).send_keys(value)
-        elif str(locator).endswith("_CLASS"):
-            self.driver.find_element(By.CLASS_NAME, configReader.readConfig("locators", locator)).send_keys(value)
-        elif str(locator).endswith("_NAME"):
-            self.driver.find_element(By.NAME, configReader.readConfig("locators", locator)).send_keys(value)
+        return self.select(locator).send_keys(value)
 
     def isDisplayed(self, locator):
-        if str(locator).endswith("_XPATH"):
-            return self.driver.find_element(By.XPATH, configReader.readConfig("locators", locator)).is_displayed()
-        elif str(locator).endswith("_CSS"):
-            return self.driver.find_element(By.CSS_SELECTOR, configReader.readConfig("locators", locator)).is_displayed()
-        elif str(locator).endswith("_ID"):
-            return self.driver.find_element(By.ID, configReader.readConfig("locators", locator)).is_displayed()
-        elif str(locator).endswith("_CLASS"):
-            return self.driver.find_element(By.CLASS_NAME, configReader.readConfig("locators", locator)).is_displayed()
-        elif str(locator).endswith("_NAME"):
-            return self.driver.find_element(By.NAME, configReader.readConfig("locators", locator)).is_displayed()
+        return self.select(locator).is_displayed()
